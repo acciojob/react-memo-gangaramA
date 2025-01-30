191190470265629a0b4508a3f3a81b853a30b6be
gangaramA <150507677+gangaramA@users.noreply.github.com>
2025-01-30 17:23:21 +0530

    Update UseMemo.js

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -1,1 +1,23 @@
+// UseMemo.js
+import React, { useMemo } from 'react';
+
+const UseMemo = ({ todos }) => {
+  const expensiveCalculation = useMemo(() => {
+    console.log('Expensive calculation running');
+    return todos.filter(todo => todo.task.includes('New'));
+  }, [todos]);
+
+  return (
+    <div>
+      <h3>Memoized Todos with "New" in their task:</h3>
+      <ul>
+        {expensiveCalculation.map(todo => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UseMemo;
 
