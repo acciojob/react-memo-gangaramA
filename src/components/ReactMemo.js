9ba9c029d396a152e90d24bb2e92660ab1e0ea55
gangaramA <150507677+gangaramA@users.noreply.github.com>
2025-01-30 17:22:55 +0530

    Update ReactMemo.js

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,1 +1,10 @@
+// ReactMemo.js
+import React from 'react';
+
+const ReactMemo = React.memo(({ task }) => {
+  console.log('Rendering task:', task);
+  return <li>{task}</li>;
+});
+
+export default ReactMemo;
 
